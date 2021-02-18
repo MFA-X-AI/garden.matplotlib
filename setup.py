e533f2de769cc02d53da0522e6a2a514829e1cc4
MFA-X-AI <68586800+MFA-X-AI@users.noreply.github.com>
2021-02-18 16:41:58 +0800

    added setup.py

---
+++ setup.py
@@ -0,0 +1,20 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    version="1.0.0,
+    author="https://github.com/kivy-garden/garden.matplotlib",
+    description="Just a setup to get this online",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/MFA-X-AI/garden.matplotlib",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
